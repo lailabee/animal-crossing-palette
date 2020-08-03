d12e86d85687be8256a40c278a54047b57e3a493
lailabee <laila.d.brahmia@gmail.com>
2020-08-02 17:23:59 -0700

    added code from k-means script

--- bot.py
+++ bot.py
@@ -1,26 +1,103 @@
 import tweepy #for tweeting
 import secrets #twitter keys
-import random #randomly generate the next target
+import math
+from PIL import Image
+import numpy as np
+import random
 
-usernames = ['___gibson','WonsonSoup','KevGuev7','its_pastable','Dr_Whomstve','sassyderrick', 
-             'LauraWhiteley23','zeekay_94','sabah_abbasi','niko_is_a_star']
+# returns the euclidean distance of two triples aka color values of two pixels
+def dist(t1,t2):
+    a = (t1[0] - t2[0]) ** 2
+    b = (t1[1] - t2[1]) ** 2
+    c = (t1[2] - t2[2]) ** 2
+    return math.sqrt(a + b + c)
 
-annoying_message = ['howdy partner!','u up?','beep beep!','honk honk!','welcome to heck buddy',
-                    'you\'re my best friend']
+# returns the average of a list of triples aka color values of pixels
 
-def tweep():
-  name = random.choice(usernames)
-  annoy = random.choice(annoying_message)
-  message = annoy + " @" + name
-  return message
+def avg(tlist):
+    a = 0
+    b = 0
+    c = 0
+    tot = len(tlist)
+    for x in tlist:
+        a += x[0]
+        b += x[1]
+        c += x[2]
+    return (a//tot,b//tot,c//tot)
 
-def tweet(message):
+def genrandomcolor():
+
+    return list(np.random.choice(range(256), size=3))
+
+def get_palette(path):
+
+	# turn image into numpy array
+
+	image = Image.open(path)
+	im = np.array(image)
+	rows,cols = im.shape
+	loops = 100 #iterations to convergence
+	k = 5 #clusters
+
+	# dict of clusters {c1,c2,c3,c4,c5}
+	# key = name
+	# value = tuple (currcolor, list of pixels in the cluster)
+
+	clusters = {
+	    "c1" : [genrandomcolor(), []],
+
+	    "c2" : [genrandomcolor(), []],
+
+	    "c3" : [genrandomcolor(), []],
+
+	    "c4" : [genrandomcolor(), []],
+
+	    "c5" : [genrandomcolor(), []]
+
+	    }
+
+	pixels = {}
+
+	for i in range(loops): #max iterations
+	    for x in range(rows):
+	        for y in range(cols): #together will iterate over each pixel value
+	            dim = (x,y)
+	            minD = ("c", math.inf) #min euc distance from a given pixel
+	            pix = im[x][y] #get color value at a given pixel
+	            for c in clusters: #for each center
+	                d = dist(pix,clusters[c][0]) #compute euclidean distance of pix to c's color val
+	                minD = (c,d) if d < minD[1] else minD #replace minD with new value if its lower
+	            clusters[minD[0]][1].append(pix) #append pix to the list for the cluster of min value in d
+	            if i == (loops - 1):
+	                pixels.update({dim : clusters[minD[0]][0]})
+	                #save each dim in a dictionary with key = dim, value = minD[0][0]
+	    for c in clusters: #for each center
+	        clist = clusters[c][1]
+	        if len(clist) != 0 and i != (loops - 1):
+	            clusters[c][0] = avg(clist) #move c to the average of the points in its cluster
+	            clusters[c][1] = [] #clear out list for next iteration
+
+	# palette of 5 colors as a numpy array for visualization
+
+	l=[]
+	for c in clusters:
+	    l.append(clusters[c][0])
+	palette = np.array([l])
+	
+	return palette
+
+def get_villager():
+
+	return vname,vicon
+
+def tweet(villager,palette,message):
   auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
   auth.set_access_token(secrets.access_token, secrets.access_token_secret)
   api = tweepy.API(auth)
   auth.secure = True
   print("Currently Tweeting.......")
-  api.update_status(status=message)
+  api.update_with_media(villager,palette,status=message)
 
 if __name__ == '__main__':
-  tweet(tweep())
+	name, icon = get_villager()
+	tweet(icon,get_palette(),name)
